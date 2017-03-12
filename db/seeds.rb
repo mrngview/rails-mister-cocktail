7b93962bf3522c81f80bcf95706b6b79899f64fd
Adrien Dubois <adrien.dubois.be@gmail.com>
2017-03-12 19:27:11 +0100

    seed done with json parser

--- db/seeds.rb
+++ db/seeds.rb
@@ -6,9 +6,23 @@
 #   movies = Movie.create([{ name: 'Star Wars' }, { name: 'Lord of the Rings' }])
 #   Character.create(name: 'Luke', movie: movies.first)
 
-Ingredient.create(name: "lemon")
-Ingredient.create(name: "ice")
-Ingredient.create(name: "mint leaves")
-Ingredient.create(name: "pineapple")
-Ingredient.create(name: "strawberry")
-Ingredient.create(name: "mango")
+require 'json'
+require 'open-uri'
+
+url = 'http://www.thecocktaildb.com/api/json/v1/1/list.php?i=list'
+file_info = open(url).read
+ingredients_list = JSON.parse(file_info)
+
+# puts "#{user["name"]} - #{user["bio"]}"
+
+# Ingredient.create(name: "lemon")
+# Ingredient.create(name: "ice")
+# Ingredient.create(name: "mint leaves")
+# Ingredient.create(name: "pineapple")
+# Ingredient.create(name: "strawberry")
+# Ingredient.create(name: "mango")
+
+ingredients_list["drinks"].each do |i|
+  Ingredient.create(name: i["strIngredient1"])
+end
+
