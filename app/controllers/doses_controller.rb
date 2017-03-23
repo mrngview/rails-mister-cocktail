9df24216715da66e0442fcace69607e409761973
Adrien Dubois <adrien.dubois.be@gmail.com>
2017-03-23 20:41:41 +0100

    Finishing backend

--- app/controllers/doses_controller.rb
+++ app/controllers/doses_controller.rb
@@ -1,10 +1,24 @@
 class DosesController < ApplicationController
   def new
+    @dose = Dose.new
+    @cocktail = Cocktail.find(params[:cocktail_id])
+  end
+
+  def create
+    @dose = Dose.new(dose_params)
+    @cocktail = Cocktail.find(params[:cocktail_id])
+    @dose.cocktail = @cocktail
+    if @dose.save
+      redirect_to cocktail_path(@cocktail)
+    else
+      render :new
+    end
   end
 
   def destroy
   end
 
-  def create
+  def dose_params
+    params.require(:dose).permit(:description, :ingredient_id)
   end
 end
